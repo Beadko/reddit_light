9ac45835f52689654892e750dd01f963388e2df4
Bea <103050835+Beadko@users.noreply.github.com>
2023-02-25 15:37:16 +0700

    Changed the funtion add to a class and started building a constructor

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import React from 'react';
 import logo from './logo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Reddit Light
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      SearchResults:[], 
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Reddit Light
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
