12c41dd77d4eaba79fb3f8959c36b68ca113fa6b
Bea <103050835+Beadko@users.noreply.github.com>
2023-03-04 14:09:11 +0700

    created a search mode for Reddit and linked it with App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.png';
 import './App.css';
 import SearchBar from './Components/SearchBar/SearchBar.js';
+import Reddit from './util/Reddit.js';
 
 class App extends React.Component {
   constructor(props) {
---
+++ src/util/Reddit.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const redirectUri = "http://localhost:3000/";
+const Reddit = {
+
+	search(term) {
+		return fetch('https://reddit.com/search.json?q=${term}',)
+		.then(response => { 
+			return response.json();
+		})
+		.then(data => {
+			data.data.children.map((data) => data.data);
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	}
+}
+export default Reddit;
