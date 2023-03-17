00e6c91dce2dc1b0ed92c6830ec07a6a469beea5
Bea <103050835+Beadko@users.noreply.github.com>
2023-03-17 19:34:55 +0700

    Reviewed the code, added semicolons

--- src/util/Reddit.js
+++ src/util/Reddit.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import axios from 'axios';
 
-/*const redirectUri = "http://localhost:3000/";
-const Reddit = {
-
-	search(term) {
-		return fetch(`https://www.reddit.com/search.json?q=${term}`
-		).then(response => { 
-			return response.json();
-		})
-		.then(data => {
-		 data.data.children.data?.map(data => data.data);
-		})
-		.catch(err => {
-			console.log(err);
-		});
-	}
-}
-export default Reddit; */
-
 export const RedditAPI = axios.create({
 	baseURL:`https://www.reddit.com`
-})
+});
 
 export const getPosts = async () => {
 	const response = await RedditAPI.get(`search.json?q=${term}`);
 	return response.data;
-}
+};
