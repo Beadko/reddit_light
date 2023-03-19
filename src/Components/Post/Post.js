099ad16309a68c4dcbbeae9120ae7e405f720ea9
Bea <103050835+Beadko@users.noreply.github.com>
2023-03-19 16:17:05 +0700

    Added a no image image in case the post doesn't have an image or image is not loading, however still need to fix

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Reddit from '../../util/Reddit.js';
+import no_image from './No-image.png';
 
 const Post = ({post}) => {
 
+  const postThumbnail= () => {
+    if (!post.thumbnail){
+      return no_image;
+      } else {
+        return post.thumbnail;
+      }
+  };
+
+  const [thumbnail, setThumbnail] = useState(postThumbnail);
+  
+  const thumbnailNotLoading =()=> {
+    setThumbnail(no_image);
+  };
+
   return (
     <article>
-      <h2>{post.title}</h2>
-      <img src={post.thumbnail} alt="post image"/>
-      <p>{post.subreddit}</p>
+      <h3>{post.title}</h3>
+      <img src={post.thumbnail} alt="post image" onError={thumbnailNotLoading}/>
     </article>
     )
 }
