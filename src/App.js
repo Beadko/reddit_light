f731e5d71a48c665d18484ff94823007c0a3aebd
Bea <103050835+Beadko@users.noreply.github.com>
2023-03-20 12:41:57 +0700

    Initiated the Reducer implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import {getPosts} from './util/Reddit.js';
+import { getPosts } from './util/Reddit.js';
 import Header from './Components/Header/Header.js';
 import SearchBar from './Components/SearchBar/SearchBar.js';
-import PostList from './Components/PostList/PostList.js'
-
+import PostList from './Components/PostList/PostList.js';
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -18,39 +17,6 @@ function App() {
         <PostList posts={posts} />
     </div>
     )
-
 }
-
-
-/*class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      search: []
-    }
-  }
-
-
-  search(term) {
-    console.log(term);
-    Reddit.search(term).then(results => {
-      return results.data;
-      this.setState({search: this.props.search});
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="TopBar">
-          <Header />
-          <SearchBar onSearch = {this.search}/>
-        </div>
-        <div className = "SearchResults"> {this.state.search}
-        </div>
-      </div>
-    );
-  }
-}*/
 
 export default App;
