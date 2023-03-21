7a01010db2287339e340ce627f0ef2f20ec400ef
Bea <103050835+Beadko@users.noreply.github.com>
2023-03-21 15:06:41 +0700

    fixed the rendering issue

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './util/Store.js';
 
-
-ReactDOM.render(
+const rootElement = 
+document.getElementById('root');
+const root = 
+createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
     <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 reportWebVitals();
