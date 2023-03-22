1ffeb380c7189e277b17521c7201eab72688e777
Bea <103050835+Beadko@users.noreply.github.com>
2023-03-22 11:21:01 +0700

    added selectAllPosts const to the slicer to be able to load the posts

--- src/Components/PostList/PostList.js
+++ src/Components/PostList/PostList.js
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react';
 import Post from '../Post/Post.js';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchPosts } from './PostsSlice.js'
+import { selectAllPosts, fetchPosts } from './PostsSlice.js'
 
 const PostList = () => {
 	const dispatch = useDispatch();
-	const posts = useSelector(state => state.posts);
+	const posts = useSelector(selectAllPosts);
 
 	useEffect(()=>{dispatch(fetchPosts())},[dispatch]);
 
-	const renderedPosts = posts.map( post => <Post key ={post.id} post={post}/> );
+	const renderedPosts = posts.map( post => 
+			<Post key ={post.id} post={post}/> 
+	);
 
 	return (
 		<div className="postList"> {renderedPosts} </div>
--- src/Components/PostList/PostsSlice.js
+++ src/Components/PostList/PostsSlice.js
@@ -20,5 +20,13 @@ export const fetchPosts = createAsyncThunk('reddit/getSubredditPosts',
 	async (subreddit) => {
 	const response = await fetch(`https://www.reddit.com/r/${subreddit}.json?limit=20`);
 	const json = await response.json();
-	return json.data.children.map(post => post.data)
+	const data = response.data.data.children;
+	const posts = data.map((post) => {
+		return { 
+			id: post.data.id,
+	        title: post.data.title,
+	        subreddit: post.data.subreddit,
+		}
+	});
+	return posts;
 });
