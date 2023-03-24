0af4926126640024a60712f6465e1700bc783b6b
Bea <103050835+Beadko@users.noreply.github.com>
2023-03-24 11:10:57 +0700

    created a subreddit Slice and added it to the Store

--- src/util/Store.js
+++ src/util/Store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import postsReducer from '../Components/PostList/PostsSlice.js'
+import postsReducer from '../Components/PostList/PostsSlice.js';
+import subredditReducer from '../Components/SideBar/SubredditSlice.js';
 
 export const store = configureStore({ 
 
 	reducer: {
 		posts: postsReducer,
+		subreddits: subredditSlice,
+
 	}
 });
