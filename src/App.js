356005ba71e76528969039a5fa7b815ef68c4801
Bea <103050835+Beadko@users.noreply.github.com>
2023-03-24 13:24:14 +0700

    Logic for the Sidebar with subreddits written, WIP

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 import Header from './Components/Header/Header.js';
 import SearchBar from './Components/SearchBar/SearchBar.js';
 import PostList from './Components/PostList/PostList.js';
+import SideBar from './Components/SideBar/SideBar.js'
 
 function App() {
   const [posts, setPosts] = useState([]);
 
   return (
     <div className="App">
-        <div className="TopBar">
+      <div className="TopBar">
           <Header />
           <SearchBar />
         </div>
         <PostList />
+        <SideBar /> 
     </div>
     )
 }
