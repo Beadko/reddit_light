35d1ff79cc61a0ef66cc691d64acbefaf1ccf322
Bea <103050835+Beadko@users.noreply.github.com>
2023-04-06 14:57:26 +0700

    moved back to the API call to test the comments

--- src/util/Reddit.js
+++ src/util/Reddit.js
@@ -2,21 +2,5 @@ import React from 'react';
 import axios from 'axios';
 
 export const RedditAPI = axios.create({
-	baseURL:`/fixtures/`
+	baseURL:`https://www.reddit.com`
 });
-
-/*export const getPosts = async (subreddit) => {
-	const response = await RedditAPI.get(`r/${subreddit}.json?limit=20`);
-
-	const data = response.data.data.children;
-
-	const posts = data.map((post) => {
-		return { 
-			id: post.data.id,
-	        title: post.data.title,
-	        subreddit: post.data.subreddit,
-		}
-	});
-
-	return posts;
-};*/
